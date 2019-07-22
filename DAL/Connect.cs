44e46eca8496e8002632c24cc9f8c63b5569b919
Olajide Fagbuji <ofagbuji@appzonegroup.com>
2019-07-22 08:48:31 +0100

    Add project files.

---
+++ DAL/Connect.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class Connect
+    {
+        private static string connectionString = @"Server=DESKTOP-DI2HUE4;Database=;User Id=sa;Password=P@ssw0rd;";
+        public void runQry(string queryString, Dictionary<string,object> args) {
+            DataTable data = new DataTable(); 
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                foreach (KeyValuePair<string, object> kv in args) {
+                   command.Parameters.AddWithValue($"@{kv.Key}", kv.Value);
+                }
+                connection.Open();
+                SqlDataAdapter da = new SqlDataAdapter(queryString, connection);
+                da.Fill(data);
+            }
+        }
+    }
+
+}
---
+++ DbConnectTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using DAL; 
+
+namespace DbConnectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Connect c = new Connect();
+            c.runQry("select * from INFORMATION_SCHEMA.TABLES", new Dictionary<string, object>());
+            Console.WriteLine("Connection Was Successful");
+        }
+    }
+}
